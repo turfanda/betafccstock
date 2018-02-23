96d59045f78122a5f727675959deb3285872334b
Glitch (betafcc-stock) <none>
2018-02-23 05:42:13 +0000

    🌜🍔 Checkpoint routes/routeHandler.js:596315/102 package.json:596315/5 .env:596315/2

--- routes/routeHandler.js
+++ routes/routeHandler.js
@@ -1,5 +1,24 @@
 const stockModel = require('../models/stock');
+const https = require("https")
 
 exports.createStock = function(req, res) {
-return 1;
+
+  
+  https.get('https://www.alphavantage.co/query?function=BATCH_STOCK_QUOTES&symbols=MSFT,FB,AAPL&apikey='+process.env.ALPHA_API_KEY, (resp) => {
+  let data = '';
+
+  resp.on('data', (chunk) => {
+    console.log(data);
+    data += chunk;
+  });
+
+  resp.on('end', () => {
+    console.log(JSON.parse(data).explanation);
+  });
+ 
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
 }
+
+
