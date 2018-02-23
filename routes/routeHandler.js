441a35056edefd977d4e1f454645f1109f35d64f
Glitch (betafcc-stock) <none>
2018-02-23 07:24:19 +0000

    🐣🐺 Checkpoint routes/routeHandler.js:596315/572 models/stock.js:596315/38

--- routes/routeHandler.js
+++ routes/routeHandler.js
@@ -1,15 +1,33 @@
 const stockModel = require('../models/stock');
 
 exports.createStock = function(req, res) {
- let stock=  new stockModel({
-  stock: req.body.stock,
+  
+  
+  stockModel.getStockByName(req.body.stock.toUpperCase(),function(err,data){
+  if (err) return res.status(501).send("Internal Error");
+  if(data==""){
+   let stock=  new stockModel({
+  stock: req.body.stock.toUpperCase(),
   likes: 1,
   IP:[req.headers['x-forwarded-for'].split(',')[0]]});
  stockModel.createStock(stock,function(err,data){
-
-            return res.send(data.likes);
+        if (err) return res.status(501).send("Internal Error");
+        else {
+            return res.status(200).send(data.likes.toString());
+        }
   
  });
+
+  }
+  
+    if(data.IP.includes(req.headers['x-forwarded-for'].split(',')[0])){
+    
+    stockModel.updateStock(1,function(err,data){});
+      
+    }
+  
+  });
+  
 
 }
 
